4f18169d1ef8ebf803e766e00092313c96e6a0a9
artonoesperson777 <151476827+artonoesperson777@users.noreply.github.com>
2026-09-17 23:03:13 -0400

    Change bare URL in uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: "/uv/service/",
-    bare: 'https://illumination.re',
+    bare: 'https://proud.lol',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
